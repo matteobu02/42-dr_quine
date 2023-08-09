04be74350138b4726a212ca5239c90bf3d453c1d
matteo bucci <matteobu02@yahoo.fr>
2023-08-10 00:28:41 +0200

    added fd protection to Grace

--- c/Grace.c
+++ c/Grace.c
@@ -4,7 +4,7 @@
 	This program will print its own source when run.
 */
 #define CHILD "Grace_kid.c"
-#define CODE(x) int x(){int fd = creat(CHILD, 0600); char str[]="#include <stdio.h>%1$c#include <fcntl.h>%1$c/*%1$c	This program will print its own source when run.%1$c*/%1$c#define CHILD %2$cGrace_kid.c%2$c%1$c#define CODE(x) int x(){int fd = creat(CHILD, 0600); char str[]=%2$c%3$s%2$c; dprintf(fd, str, 10, 34, str); return 0;}%1$c#define MAIN main%1$c%1$cCODE(MAIN)%1$c"; dprintf(fd, str, 10, 34, str); return 0;}
+#define CODE(x) int x(){int fd = creat(CHILD, 0600); if (fd == -1) return 1; char str[]="#include <stdio.h>%1$c#include <fcntl.h>%1$c/*%1$c	This program will print its own source when run.%1$c*/%1$c#define CHILD %2$cGrace_kid.c%2$c%1$c#define CODE(x) int x(){int fd = creat(CHILD, 0600); if (fd == -1) return 1; char str[]=%2$c%3$s%2$c; dprintf(fd, str, 10, 34, str); return 0;}%1$c#define MAIN main%1$c%1$cCODE(MAIN)%1$c"; dprintf(fd, str, 10, 34, str); return 0;}
 #define MAIN main
 
 CODE(MAIN)
