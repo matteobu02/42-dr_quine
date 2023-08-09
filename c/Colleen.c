71ff2e949aab08d5ceab3a66e25ed5aa57b2c39d
matteo bucci <matteobu02@yahoo.fr>
2023-08-09 21:03:33 +0200

    formated comments as specified in subject

--- c/Colleen.c
+++ c/Colleen.c
@@ -1,9 +1,14 @@
-#include <stdio.h> // comment outside main.
+#include <stdio.h>
+/*
+	Comment outside of main.
+*/
 void dummy(){return;}
 int main()
 {
-	// comment in main.
+	/*
+		Comment inside main.
+	*/
 	dummy();
-	char str[]="#include<stdio.h> // comment outside main.%1$cvoid dummy(){return;}%1$cint main()%1$c{%1$c	// comment in main.%1$c	dummy();%1$c	char str[]=%2$c%3$s%2$c;%1$c	printf(str, 10, 34, str);%1$c}%1$c";
+	char str[]="#include <stdio.h>%1$c/*%1$c	Comment outside of main.%1$c*/%1$cvoid dummy(){return;}%1$cint main()%1$c{%1$c	/*%1$c		Comment inside main.%1$c	*/%1$c	dummy();%1$c	char str[]=%2$c%3$s%2$c;%1$c	printf(str, 10, 34, str);%1$c}%1$c";
 	printf(str, 10, 34, str);
 }
