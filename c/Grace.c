0c556c532f261c7d21e594485b9e1d2914c5a9d7
matteo bucci <matteobu02@yahoo.fr>
2023-08-09 15:01:03 +0200

    Grace c done

--- c/Grace.c
+++ c/Grace.c
@@ -1,2 +1,8 @@
-int main()
-{return 0;}
+#include <stdio.h>
+#include <fcntl.h>
+// the comment.
+#define CHILD "Grace_kid.c"
+#define CODE(x) int x(){int fd = creat(CHILD, 0600); char str[]="#include <stdio.h>%1$c#include <fcntl.h>%1$c// the comment.%1$c#define CHILD %2$cGrace_kid.c%2$c%1$c#define CODE(x) int x(){int fd = creat(CHILD, 0600); char str[]=%2$c%3$s%2$c; dprintf(fd, str, 10, 34, str); return 0;}%1$c#define MAIN main%1$c%1$cCODE(MAIN)%1$c"; dprintf(fd, str, 10, 34, str); return 0;}
+#define MAIN main
+
+CODE(MAIN)
