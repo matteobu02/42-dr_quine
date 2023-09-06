550e7e0a568810b34ddf5c6f4be1e9a15812d57b
matteo bucci <matteobu02@yahoo.fr>
2023-09-06 02:38:55 +0200

    Sully.s fixed for eval

--- asm/Sully.s
+++ asm/Sully.s
@@ -6,7 +6,6 @@ extern creat
 extern system
 extern exit
 
-extern printf
 section .bss
 final_filename: resb 32
 final_exec: resb 32
@@ -14,11 +13,10 @@ final_cmd: resb 256
 
 section .data
 i: dd 5
-mystr: db "global _start%1$cextern sprintf%1$cextern dprintf%1$cextern open%1$cextern creat%1$cextern system%1$cextern exit%1$c%1$csection .bss%1$cfinal_filename: resb 32%1$cfinal_exec: resb 32%1$cfinal_cmd: resb 256%1$c%1$csection .data%1$ci: dd %3$d%1$cmystr: db %2$c%4$s%2$c,0%1$cfilename_patt: db %2$cSully_%%d.s%2$c,0%1$cexec_patt: db %2$cSully_%%d%2$c,0%1$ccmd_patt: db %2$cnasm -felf64 %%1$s && ld --dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc %%2$s.o -o %%2$s && ./%%2$s%2$c,0%1$c%1$csection .text%1$c_start:%1$c	cmp DWORD [i], 0%1$c	jng .endprog%1$c%1$c	mov edx, DWORD [i]%1$c	mov rsi, filename_patt%1$c	mov rdi, final_filename%1$c	call sprintf%1$c%1$c	mov rsi, 0%1$c	mov rdi, final_filename%1$c	call open%1$c	cmp rax, -1%1$c	jne .file_ready%1$c%1$c	mov edx, DWORD [i]%1$c	dec edx%1$c	mov DWORD [i], edx%1$c	mov rsi, filename_patt%1$c	mov rdi, final_filename%1$c	call sprintf%1$c%1$c.file_ready:%1$c	mov rsi, 0600o%1$c	mov rdi, final_filename%1$c	call creat%1$c	cmp rax, -1%1$c	je .error%1$c%1$c	mov r9, mystr%1$c	mov r8d, DWORD [i]%1$c	mov rcx, 34%1$c	mov rdx, 10%1$c	mov rsi, mystr%1$c	mov rdi, rax%1$c	call dprintf%1$c%1$c	mov edx, DWORD [i]%1$c	mov rsi, exec_patt%1$c	mov rdi, final_exec%1$c	call sprintf%1$c%1$c	mov rcx, final_exec%1$c	mov rdx, final_filename%1$c	mov rsi, cmd_patt%1$c	mov rdi, final_cmd%1$c	call sprintf%1$c%1$c	mov rdi, final_cmd%1$c	call system%1$c%1$c.endprog:%1$c	xor rdi, rdi%1$c	call exit wrt ..plt%1$c%1$c.error:%1$c	mov rdi, 1%1$c	call exit wrt ..plt%1$c",0
+mystr: db "global _start%1$cextern sprintf%1$cextern dprintf%1$cextern open%1$cextern creat%1$cextern system%1$cextern exit%1$c%1$csection .bss%1$cfinal_filename: resb 32%1$cfinal_exec: resb 32%1$cfinal_cmd: resb 256%1$c%1$csection .data%1$ci: dd %3$d%1$cmystr: db %2$c%4$s%2$c,0%1$cfilename_patt: db %2$cSully_%%d.s%2$c,0%1$cexec_patt: db %2$cSully_%%d%2$c,0%1$ccmd_patt: db %2$cnasm -felf64 %%1$s && ld --dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc %%2$s.o -o %%2$s && ./%%2$s%2$c,0%1$c%1$csection .text%1$c_start:%1$c	cmp DWORD [i], 0%1$c	jng .endprog%1$c%1$c	mov edx, [i]%1$c	mov rsi, filename_patt%1$c	mov rdi, final_filename%1$c	call sprintf%1$c%1$c	mov rsi, 0%1$c	mov rdi, final_filename%1$c	call open%1$c	cmp eax, -1%1$c	je .file_ready%1$c%1$c	sub [i], DWORD 1%1$c	mov edx, DWORD [i]%1$c	mov rsi, filename_patt%1$c	mov rdi, final_filename%1$c	call sprintf%1$c%1$c.file_ready:%1$c	mov rsi, 0600o%1$c	mov rdi, final_filename%1$c	call creat%1$c	cmp eax, -1%1$c	je .error%1$c%1$c	mov r9, mystr%1$c	mov r8, [i]%1$c	mov rcx, 34%1$c	mov rdx, 10%1$c	mov rsi, mystr%1$c	mov rdi, rax%1$c	call dprintf%1$c%1$c	mov rdx, [i]%1$c	mov rsi, exec_patt%1$c	mov rdi, final_exec%1$c	call sprintf%1$c%1$c	mov rcx, final_exec%1$c	mov rdx, final_filename%1$c	mov rsi, cmd_patt%1$c	mov rdi, final_cmd%1$c	call sprintf%1$c%1$c	mov rdi, final_cmd%1$c	call system%1$c%1$c.endprog:%1$c	xor rdi, rdi%1$c	call exit wrt ..plt%1$c%1$c.error:%1$c	mov rdi, 1%1$c	call exit wrt ..plt%1$c",0
 filename_patt: db "Sully_%d.s",0
 exec_patt: db "Sully_%d",0
 cmd_patt: db "nasm -felf64 %1$s && ld --dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc %2$s.o -o %2$s && ./%2$s",0
-deb: db "%d",10,0
 
 section .text
 _start:
@@ -33,8 +31,8 @@ _start:
 	mov rsi, 0
 	mov rdi, final_filename
 	call open
-	cmp rax, 0
-	jl .file_ready
+	cmp eax, -1
+	je .file_ready
 
 	sub [i], DWORD 1
 	mov edx, DWORD [i]
@@ -46,7 +44,7 @@ _start:
 	mov rsi, 0600o
 	mov rdi, final_filename
 	call creat
-	cmp rax, -1
+	cmp eax, -1
 	je .error
 
 	mov r9, mystr
