7c537901c5b556ef1db340452352cc910ed1e46e
matteo bucci <matteobu02@yahoo.fr>
2023-09-06 11:41:52 +0200

    removed 'wrt ..plt' from asm files

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -5,7 +5,7 @@ extern exit
 ; comment outside of main.
 
 section .data
-mystr: db "global main%1$cextern printf%1$cextern exit%1$c%1$c; comment outside of main.%1$c%1$csection .data%1$cmystr: db %2$c%3$s%2$c%1$c%1$csection .text%1$c%1$cdummy:%1$c	ret%1$c%1$cmain:%1$c	; comment in main.%1$c	call dummy%1$c	mov rcx, mystr%1$c	mov rdx, 34%1$c	mov rsi, 10%1$c	mov rdi, mystr%1$c	call printf wrt ..plt%1$c%1$c	xor rdi, rdi%1$c	call exit%1$c"
+mystr: db "global main%1$cextern printf%1$cextern exit%1$c%1$c; comment outside of main.%1$c%1$csection .data%1$cmystr: db %2$c%3$s%2$c%1$c%1$csection .text%1$c%1$cdummy:%1$c	ret%1$c%1$cmain:%1$c	; comment in main.%1$c	call dummy%1$c	mov rcx, mystr%1$c	mov rdx, 34%1$c	mov rsi, 10%1$c	mov rdi, mystr%1$c	call printf%1$c%1$c	xor rdi, rdi%1$c	call exit%1$c"
 
 section .text
 
@@ -19,7 +19,7 @@ main:
 	mov rdx, 34
 	mov rsi, 10
 	mov rdi, mystr
-	call printf wrt ..plt
+	call printf
 
 	xor rdi, rdi
 	call exit
--- asm/Sully.s
+++ asm/Sully.s
@@ -13,7 +13,7 @@ final_cmd: resb 256
 
 section .data
 i: dd 5
-mystr: db "global _start%1$cextern sprintf%1$cextern dprintf%1$cextern open%1$cextern creat%1$cextern system%1$cextern exit%1$c%1$csection .bss%1$cfinal_filename: resb 32%1$cfinal_exec: resb 32%1$cfinal_cmd: resb 256%1$c%1$csection .data%1$ci: dd %3$d%1$cmystr: db %2$c%4$s%2$c,0%1$cfilename_patt: db %2$cSully_%%d.s%2$c,0%1$cexec_patt: db %2$cSully_%%d%2$c,0%1$ccmd_patt: db %2$cnasm -felf64 %%1$s && ld --dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc %%2$s.o -o %%2$s && ./%%2$s%2$c,0%1$c%1$csection .text%1$c_start:%1$c	cmp DWORD [i], 0%1$c	jng .endprog%1$c%1$c	mov edx, [i]%1$c	mov rsi, filename_patt%1$c	mov rdi, final_filename%1$c	call sprintf%1$c%1$c	mov rsi, 0%1$c	mov rdi, final_filename%1$c	call open%1$c	cmp eax, -1%1$c	je .file_ready%1$c%1$c	sub [i], DWORD 1%1$c	mov edx, DWORD [i]%1$c	mov rsi, filename_patt%1$c	mov rdi, final_filename%1$c	call sprintf%1$c%1$c.file_ready:%1$c	mov rsi, 0600o%1$c	mov rdi, final_filename%1$c	call creat%1$c	cmp eax, -1%1$c	je .error%1$c%1$c	mov r9, mystr%1$c	mov r8, [i]%1$c	mov rcx, 34%1$c	mov rdx, 10%1$c	mov rsi, mystr%1$c	mov rdi, rax%1$c	call dprintf%1$c%1$c	mov rdx, [i]%1$c	mov rsi, exec_patt%1$c	mov rdi, final_exec%1$c	call sprintf%1$c%1$c	mov rcx, final_exec%1$c	mov rdx, final_filename%1$c	mov rsi, cmd_patt%1$c	mov rdi, final_cmd%1$c	call sprintf%1$c%1$c	mov rdi, final_cmd%1$c	call system%1$c%1$c.endprog:%1$c	xor rdi, rdi%1$c	call exit wrt ..plt%1$c%1$c.error:%1$c	mov rdi, 1%1$c	call exit wrt ..plt%1$c",0
+mystr: db "global _start%1$cextern sprintf%1$cextern dprintf%1$cextern open%1$cextern creat%1$cextern system%1$cextern exit%1$c%1$csection .bss%1$cfinal_filename: resb 32%1$cfinal_exec: resb 32%1$cfinal_cmd: resb 256%1$c%1$csection .data%1$ci: dd %3$d%1$cmystr: db %2$c%4$s%2$c,0%1$cfilename_patt: db %2$cSully_%%d.s%2$c,0%1$cexec_patt: db %2$cSully_%%d%2$c,0%1$ccmd_patt: db %2$cnasm -felf64 %%1$s && ld --dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc %%2$s.o -o %%2$s && ./%%2$s%2$c,0%1$c%1$csection .text%1$c_start:%1$c	cmp DWORD [i], 0%1$c	jng .endprog%1$c%1$c	mov edx, [i]%1$c	mov rsi, filename_patt%1$c	mov rdi, final_filename%1$c	call sprintf%1$c%1$c	mov rsi, 0%1$c	mov rdi, final_filename%1$c	call open%1$c	cmp eax, -1%1$c	je .file_ready%1$c%1$c	sub [i], DWORD 1%1$c	mov edx, DWORD [i]%1$c	mov rsi, filename_patt%1$c	mov rdi, final_filename%1$c	call sprintf%1$c%1$c.file_ready:%1$c	mov rsi, 0600o%1$c	mov rdi, final_filename%1$c	call creat%1$c	cmp eax, -1%1$c	je .error%1$c%1$c	mov r9, mystr%1$c	mov r8, [i]%1$c	mov rcx, 34%1$c	mov rdx, 10%1$c	mov rsi, mystr%1$c	mov rdi, rax%1$c	call dprintf%1$c%1$c	mov rdx, [i]%1$c	mov rsi, exec_patt%1$c	mov rdi, final_exec%1$c	call sprintf%1$c%1$c	mov rcx, final_exec%1$c	mov rdx, final_filename%1$c	mov rsi, cmd_patt%1$c	mov rdi, final_cmd%1$c	call sprintf%1$c%1$c	mov rdi, final_cmd%1$c	call system%1$c%1$c.endprog:%1$c	xor rdi, rdi%1$c	call exit%1$c%1$c.error:%1$c	mov rdi, 1%1$c	call exit%1$c",0
 filename_patt: db "Sully_%d.s",0
 exec_patt: db "Sully_%d",0
 cmd_patt: db "nasm -felf64 %1$s && ld --dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc %2$s.o -o %2$s && ./%2$s",0
@@ -71,8 +71,8 @@ _start:
 
 .endprog:
 	xor rdi, rdi
-	call exit wrt ..plt
+	call exit
 
 .error:
 	mov rdi, 1
-	call exit wrt ..plt
+	call exit
