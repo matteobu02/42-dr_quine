e3a21dde2a3c449c8f86960e44cd6f9f8832f3e3
matteo bucci <matteobu02@yahoo.fr>
2023-09-09 20:34:17 +0200

    moved data section definitions into macro

--- asm/Grace.s
+++ asm/Grace.s
@@ -25,11 +25,11 @@ _start:
 .error:
 	mov rdi, 1
 	call exit
-%endmacro
 
 section .data
 child: db "Grace_kid.s", 0
-mystr: db "global _start%1$cextern creat%1$cextern dprintf%1$cextern exit%1$c; my comment is here.%1$c%1$c%%macro CODE 0%1$c_start:%1$c	mov rsi, 0600o%1$c	mov rdi, child%1$c	call creat%1$c	cmp rax, -1%1$c	je .error%1$c%1$c	mov r8, mystr%1$c	mov rcx, 34%1$c	mov rdx, 10%1$c	mov rsi, mystr%1$c	mov rdi, rax%1$c	call dprintf%1$c%1$c	xor rdi, rdi%1$c	call exit%1$c%1$c.error:%1$c	mov rdi, 1%1$c	call exit%1$c%%endmacro%1$c%1$csection .data%1$cchild: db %2$cGrace_kid.s%2$c, 0%1$cmystr: db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cCODE%1$c", 0
+mystr: db "global _start%1$cextern creat%1$cextern dprintf%1$cextern exit%1$c; my comment is here.%1$c%1$c%%macro CODE 0%1$c_start:%1$c	mov rsi, 0600o%1$c	mov rdi, child%1$c	call creat%1$c	cmp rax, -1%1$c	je .error%1$c%1$c	mov r8, mystr%1$c	mov rcx, 34%1$c	mov rdx, 10%1$c	mov rsi, mystr%1$c	mov rdi, rax%1$c	call dprintf%1$c%1$c	xor rdi, rdi%1$c	call exit%1$c%1$c.error:%1$c	mov rdi, 1%1$c	call exit%1$c%1$csection .data%1$cchild: db %2$cGrace_kid.s%2$c, 0%1$cmystr: db %2$c%3$s%2$c, 0%1$c%%endmacro%1$c%1$csection .text%1$cCODE%1$c", 0
+%endmacro
 
 section .text
 CODE
